4db6b086e6c8ebfc27257810bcbc99e4fbd3adc5
d.zulin <d.zulin@g.nsu.ru>
2020-11-03 15:26:49 +0700

    Added byte counter, so we do not decode more than needed

--- coder.cpp
+++ coder.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include "lodepng/lodepng.h"
 
+#define LAST_BYTE_MASK 0x03u
+
 int main(int argc, char** argv) {
     if (argc != 3) {
         std::cerr << "Usage: " << argv[0] << " <image_to_code_into.png> <crypto_text>" << std::endl;
@@ -21,21 +23,28 @@ int main(int argc, char** argv) {
         std::cerr << "Could not open text file" << std::endl;
         return 1;
     }
-
     size_t total_bytes = image_data.size() / 4;
-    size_t written_bytes = 0;
+    uint64_t written_bytes = 8;
     unsigned char input_buffer;
     while (!text.eof() && written_bytes < total_bytes) {
         text.read(reinterpret_cast<char *>(&input_buffer), 1);
-        image_data[written_bytes * 4] &= 0b11111100u;
-        image_data[written_bytes * 4] |= (0b00000011u & (input_buffer >> 0u));
-        image_data[written_bytes * 4 + 1] &= 0b11111100u;
-        image_data[written_bytes * 4 + 1] |= (0b00000011u & (input_buffer >> 2u));
-        image_data[written_bytes * 4 + 2] &= 0b11111100u;
-        image_data[written_bytes * 4 + 2] |= (0b00000011u & (input_buffer >> 4u));
-        image_data[written_bytes * 4 + 3] &= 0b11111100u;
-        image_data[written_bytes * 4 + 3] |= (0b00000011u & (input_buffer >> 6u));
+        if (text.eof()) {
+            break;
+        }
+        image_data[written_bytes * 4] &= !LAST_BYTE_MASK;
+        image_data[written_bytes * 4] |= (LAST_BYTE_MASK & (input_buffer >> 0u));
+        image_data[written_bytes * 4 + 1] &= !LAST_BYTE_MASK;
+        image_data[written_bytes * 4 + 1] |= (LAST_BYTE_MASK & (input_buffer >> 2u));
+        image_data[written_bytes * 4 + 2] &= !LAST_BYTE_MASK;
+        image_data[written_bytes * 4 + 2] |= (LAST_BYTE_MASK & (input_buffer >> 4u));
+        image_data[written_bytes * 4 + 3] &= !LAST_BYTE_MASK;
+        image_data[written_bytes * 4 + 3] |= (LAST_BYTE_MASK & (input_buffer >> 6u));
         written_bytes++;
+    }
+    written_bytes -= 8;
+    for (int bit_tuples = 0; bit_tuples < 32; ++bit_tuples) {
+        image_data[bit_tuples] &= !LAST_BYTE_MASK;
+        image_data[bit_tuples] |= (LAST_BYTE_MASK & (written_bytes >> (2u * bit_tuples)));
     }
     text.close();
     if (written_bytes >= total_bytes) {
--- decoder.cpp
+++ decoder.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include "lodepng/lodepng.h"
 
+#define LAST_BYTE_MASK 0x03u
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <image_to_decode_into.png>" << std::endl;
@@ -18,15 +20,21 @@ int main(int argc, char** argv) {
 
     std::ofstream output("result.txt", std::ios_base::out & std::ios_base::binary);
 
-    size_t total_bytes = image_data.size();
-    size_t processed_bytes = 0;
+    size_t total_bytes = image_data.size() / 4;
+
+    uint64_t saved_bytes = 0;
+    for (int bit_tuples = 0; bit_tuples < 32; ++bit_tuples) {
+        saved_bytes += (image_data[bit_tuples] & LAST_BYTE_MASK) << (bit_tuples * 2u);
+    }
+
+    size_t processed_bytes = 8;
     unsigned char collect_result;
-    while (processed_bytes < total_bytes / 4) {
+    while (processed_bytes - 8 < saved_bytes && processed_bytes < total_bytes) {
         collect_result = 0;
-        collect_result += image_data[processed_bytes * 4] & 0b00000011u;
-        collect_result += (image_data[processed_bytes * 4 + 1] & 0b00000011u) << 2u;
-        collect_result += (image_data[processed_bytes * 4 + 2] & 0b00000011u) << 4u;
-        collect_result += (image_data[processed_bytes * 4 + 3] & 0b00000011u) << 6u;
+        collect_result += image_data[processed_bytes * 4] & LAST_BYTE_MASK;
+        collect_result += (image_data[processed_bytes * 4 + 1] & LAST_BYTE_MASK) << 2u;
+        collect_result += (image_data[processed_bytes * 4 + 2] & LAST_BYTE_MASK) << 4u;
+        collect_result += (image_data[processed_bytes * 4 + 3] & LAST_BYTE_MASK) << 6u;
         output.write(reinterpret_cast<const char *>(&collect_result), 1);
         processed_bytes++;
     }
